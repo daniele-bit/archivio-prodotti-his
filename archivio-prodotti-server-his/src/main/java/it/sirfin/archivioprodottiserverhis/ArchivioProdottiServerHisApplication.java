42590e4b353fc6baa5d88511299c657d1c4ce42d
daniele-bit <daniele.fas95@gmail.com>
2021-03-05 14:58:27 +0100

    Risolto Definire application properties lato server #8

---
+++ archivio-prodotti-server-his/src/main/java/it/sirfin/archivioprodottiserverhis/ArchivioProdottiServerHisApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivioprodottiserverhis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioProdottiServerHisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioProdottiServerHisApplication.class, args);
+	}
+
+}
---
+++ archivio-prodotti-server-his/src/test/java/it/sirfin/archivioprodottiserverhis/ArchivioProdottiServerHisApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivioprodottiserverhis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivioProdottiServerHisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
