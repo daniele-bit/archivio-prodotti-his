82e5cf577cf11b919def4089078263a7f33ab814
nicoloabatecola <78588208+nicoloabatecola@users.noreply.github.com>
2021-03-07 10:28:32 +0100

    Risolto #6

--- archivio-prodotti-client-his/src/app/app.component.ts
+++ archivio-prodotti-client-his/src/app/app.component.ts
@@ -14,47 +14,50 @@ export class AppComponent {
   prodotto = new Prodotto();
   criterioRicerca = "";
   prodotti: Prodotto[] = [];
+  prodottoSconto: Prodotto;
 
-  constructor(private http: HttpClient) {
-    this.aggiorna(this.criterioRicerca);
+constructor(private http: HttpClient) {
+  this.aggiorna(this.criterioRicerca);
+}
+
+aggiungi() {
+  let dto = new ProdottoDto();
+  dto.prodotto = this.prodotto;
+  this.http.post<ListaProdottiDto>("http://localhost:8080/aggiungi-prodotto", dto)
+    .subscribe(p => this.prodotti = p.listaProdotti);
+  this.prodotto = new Prodotto();
+}
+ricerca() {
+  let criterio = new RicercaDto();
+  criterio.ricerca = this.criterioRicerca;
+
+  this.http.post<ListaProdottiDto>("http://localhost:8080/ricerca-prodotto", criterio)
+    .subscribe(p => this.prodotti = p.listaProdotti);
+}
+
+cancella(p: Prodotto) {
+  let dto = new ProdottoDto();
+  dto.prodotto = p;
+
+  let oss = this.http.post<ListaProdottiDto>("http://localhost:8080/cancella-prodotto", dto);
+  oss.subscribe(p => this.prodotti = p.listaProdotti);
+}
+
+aggiorna(ricerca: string) {
+  let ricercaDto = new RicercaDto();
+  ricercaDto.ricerca = ricerca;
+
+  if (ricerca == "") {
+    this.http.get<ListaProdottiDto>("http://localhost:8080/aggiorna-lista")
+      .subscribe(c => {
+        this.prodotti = c.listaProdotti;
+      });
+  } else {
+    console.log("Errore criterio");
   }
+}
 
-  aggiungi() {
-    let dto = new ProdottoDto();
-    dto.prodotto = this.prodotto;
-    this.http.post<ListaProdottiDto>("http://localhost:8080/aggiungi-prodotto", dto)
-      .subscribe(p => this.prodotti = p.listaProdotti);
-    this.prodotto = new Prodotto();
-  }
-  ricerca() {
-    let criterio = new RicercaDto();
-    criterio.ricerca = this.criterioRicerca;
-
-    this.http.post<ListaProdottiDto>("http://localhost:8080/ricerca-prodotto", criterio)
-      .subscribe(p => this.prodotti = p.listaProdotti);
-  }
-
-  cancella(p: Prodotto) {
-    let dto = new ProdottoDto();
-    dto.prodotto = p;
-
-    let oss = this.http.post<ListaProdottiDto>("http://localhost:8080/cancella-prodotto", dto);
-    oss.subscribe(p => this.prodotti = p.listaProdotti);
-  }
-
-  aggiorna(ricerca: string) {
-    let ricercaDto = new RicercaDto();
-    ricercaDto.ricerca = ricerca;
-
-    if (ricerca == "") {
-      this.http.get<ListaProdottiDto>("http://localhost:8080/aggiorna-lista")
-        .subscribe(c => {
-          this.prodotti = c.listaProdotti;
-        });
-    } else {
-      console.log("Errore criterio");
-    }
-  }
-
-  calcolaSconto(p: Prodotto) { }
+calcolaSconto(p: Prodotto) {
+  this.prodottoSconto = p;
+}
 }
